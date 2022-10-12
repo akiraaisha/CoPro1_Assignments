c4c88893e7afa8d8dd5dd55c2491789b5b847044
Julian Lugod <julianlugod@yahoo.com>
2022-10-12 17:29:14 +0800

    Update Factorial_Assignment.cpp

--- Factorial_Assignment.cpp
+++ Factorial_Assignment.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 
 int main() {
-
     int x, fact = 1, n;
 
     cout << "Enter a number to find factorial: ";
-
     cin >> n;
 
     for (x = 1; x <= n; x++)
-
         fact = fact * x;
-
-    cout << "Factorial of " << n << " is: " <<  fact;
+    
+    cout << "The factorial of the given number is: " << fact;
 
     return 0;
 
