12d9b37ab900249fec2c8a30befdf2ed506467aa
Julian Lugod <6212018+akiraaisha@users.noreply.github.com>
2023-02-09 20:29:48 +0800

    Update PaySlipGenerator.java

--- PaySlipGenerator.java
+++ PaySlipGenerator.java
@@ -1,16 +1,14 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 class PayslipGenerator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0.00");
 
         System.out.print("Enter employee name: ");
         String name = sc.nextLine();
 
         System.out.print("Enter hourly rate: ");
-        double hourlyRate = sc.nextDouble();
+        int hourlyRate = sc.nextInt();
 
         System.out.print("Enter Total of hours worked: ");
         int hoursWorked = sc.nextInt();
@@ -18,15 +16,15 @@ class PayslipGenerator {
         System.out.print("Enter Total Tardiness in Hour: ");
         int tardiness = sc.nextInt();
 
-        double grossPay = hoursWorked * hourlyRate;
-        double netPay = grossPay - (hourlyRate * tardiness);
+        int grossPay = hoursWorked * hourlyRate;
+        int netPay = grossPay - (hourlyRate * tardiness);
 
         System.out.println("\nPaySlip");
         System.out.println("Employee Name: " + name);
-        System.out.println("Rate/hr: " + df.format(hourlyRate));
+        System.out.println("Rate/hr: " + hourlyRate);
         System.out.println("Hours Worked: " + hoursWorked);
-        System.out.println("Gross Pay: PHP " + df.format(grossPay));
-        System.out.println("Total Tardiness: " + tardiness);
-        System.out.println("Net Pay: PHP " + df.format(netPay));
+        System.out.println("Gross Pay: PHP " + grossPay);
+        System.out.println("Total Tardiness: " + tardiness * hourlyRate);
+        System.out.println("Net Pay: PHP " + netPay);
     }
 }
